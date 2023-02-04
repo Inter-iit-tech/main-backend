2532b4a017171574ca91d5404a8a5d859d2fe78e
Sarthak <61661622+sg60692@users.noreply.github.com>
2023-02-04 21:06:56 +0530

    Del Pickup

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -5,6 +5,7 @@ const notificationController = require("./../controllers/notificationController"
 router.get("/details", adminController.adminDetails);
 router.get("/details-db/:reqQuery", adminController.getDetails);
 router.post("/add-pickup", adminController.addPickup);
+router.post("/del-pickup", adminController.deletePickup);
 router.post("/notification", notificationController.sendNotification);
 
 module.exports = router;
