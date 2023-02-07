e0545d436117641bdb178f62778dee3e3da40706
PranayB003 <pranayborgohain@gmail.com>
2023-02-08 04:15:14 +0530

    prettier formatted riderController

--- controllers/riderController.js
+++ controllers/riderController.js
@@ -80,51 +80,46 @@ const markOrderStatus = catchAsync(async (req, res, next) => {
   const location = req.body.order.location;
   const userLocation = req.body.order.riderLocation;
 
-
   if (!riderID || !orderID || !status) {
     return next(new AppError("Bad Request", 400));
   }
 
-const rider = await Rider.findById(riderID);
+  const rider = await Rider.findById(riderID);
 
+  if (!rider) {
+    return next(new AppError("Rider not found", 404));
+  }
 
-if (!rider) {
-  return next(new AppError("Rider not found", 404));
-}
+  const currentDeliveryLocation = rider?.tours?.[0]?.[0]?.orderId;
+  console.log({ orderID, currentDeliveryLocation });
+  if (!currentDeliveryLocation.equals(orderID)) {
+    return next(new AppError("This is not the next deliveryLocation", 400));
+  }
 
-const currentDeliveryLocation = rider?.tours?.[0]?.[0]?.orderId;
-console.log({orderID, currentDeliveryLocation});
-if (!currentDeliveryLocation.equals(orderID)) {
-  return next(new AppError("This is not the next deliveryLocation", 400));
-}
+  const tours = rider?.tours;
 
+  console.log({ tours });
 
+  tours[0].splice(0, 1);
 
-const tours = rider?.tours;
-
-console.log({tours});
-
-tours[0].splice(0,1);
-
-if (tours[0].length < 0) {
-  tours.splice(0,1);
-}
-console.log({tours});
-
-
+  if (tours[0].length < 0) {
+    tours.splice(0, 1);
+  }
+  console.log({ tours });
 
   const updateObject = { isDelivered: status, isFakeAttempt: false };
-  
-    // TODO: Get location of rider and check if the rider is the acceptable distance from the drop location.
-    const dist = getDistanceFromLatLonInKm(location, userLocation);
-    if (dist > acceptableDistanceInKm) {
-      updateObject.isFakeAttempt = true;
-      console.log("This is a fake attempt");
-    }
+
+  // TODO: Get location of rider and check if the rider is the acceptable distance from the drop location.
+  const dist = getDistanceFromLatLonInKm(location, userLocation);
+  if (dist > acceptableDistanceInKm) {
+    updateObject.isFakeAttempt = true;
+    console.log("This is a fake attempt");
+  }
 
   const updateOrder = await Order.findByIdAndUpdate(orderID, updateObject);
-  const updateRider = await Rider.findByIdAndUpdate(riderID, {tours: newTour})
-
+  const updateRider = await Rider.findByIdAndUpdate(riderID, {
+    tours: newTour,
+  });
 
   res.status(200).json({
     message: "Order status updated successfully",
